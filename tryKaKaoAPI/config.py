3dcecd1926ffd16899cfe6dcb5ddd6c27350969d
Leeyoonhu <316902@naver.com>
2022-10-04 10:01:42 +0900

    add 20221004

---
+++ tryKaKaoAPI/config.py
@@ -0,0 +1,16 @@
+# 소셜 로그인을 위한 키 정보들 저장된 config.py
+
+# * kakao *
+# 내 REST API 키(client_id) : d23a3184f03474573dded85b7450aa27
+# 내 client secret 키 : kVbMBj8DEwjCkYOIHhxzS94vOUFshJTy
+# 내 redirect_uri 키 : http://localhost:8080/
+
+CLIENT_ID = "d23a3184f03474573dded85b7450aa27"
+CLIENT_SECRET = "kVbMBj8DEwjCkYOIHhxzS94vOUFshJTy"
+REDIRECT_URI = "http://localhost:8000/"
+
+
+# 사용자의 service page 접근
+@app.route("/")
+def index():
+    return render_template('index.html')
